eaee567f016f05515f290cc35667839889163e3b
lamdz1102 <lamndhhe182252@fpt.edu.vn>
2026-06-04 14:04:20 +0700

    Thêm package gọi API trong flutter

--- frontend/lib/screens/splash_screen.dart
+++ frontend/lib/screens/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
